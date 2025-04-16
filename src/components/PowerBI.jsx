559f0f79570212a26daefc78c9866a27e525567c
Omomene Iwelomen <iwelomene@gmail.com>
2025-04-16 15:56:01 +0200

    analytics

--- src/components/PowerBI.jsx
+++ src/components/PowerBI.jsx
@@ -1,17 +1,36 @@
-function PowerBIEmbed() {
-    return (
-      <div className="w-full h-screen">
-        <iframe 
-          title="Power BI Report"
-          width="100%" 
-          height="100%"
-          src="https://app.powerbi.com/view?r=eyJrIjoiNzk1NWYxYWYtM2JiYi00YWQ3LWFlMjctNGM0MTQzNmU0MTA3IiwidCI6IjEwOGJjODY0LWNkZjUtNGVjMy04YjdjLTRlYjA2YmUxYjQxZCIsImMiOjl9"
-          frameBorder="0"
-          allowFullScreen
-        />
-      </div>
-    );
-  }
+function PowerBIEmbed({ src }) {
+  return (
+    <div className="w-full h-[600px] mb-8 rounded-lg overflow-hidden shadow-lg">
+      <iframe
+        title="Power BI Report"
+        width="100%"
+        height="100%"
+        src={src}
+        frameBorder="0"
+        allowFullScreen
+      />
+    </div>
+  );
+}
+
+export default PowerBIEmbed;
+
+
+// function PowerBIEmbed() {
+//     return (
+//       <div className="w-full h-screen">
+//         <iframe 
+//           title="Power BI Report"
+//           width="100%" 
+//           height="100%"
+//           src="https://app.powerbi.com/reportEmbed?reportId=171f5c1b-20fa-4251-98a9-54d0604373db&autoAuth=true&ctid=108bc864-cdf5-4ec3-8b7c-4eb06be1b41d&pageName=089f0ad2ab64b37d6166&navContentPaneEnabled=false"
+//           frameBorder="0"
+//           allowFullScreen
+//         />
+//       </div>
+        
+//     );
+//   }
   
-  export default PowerBIEmbed;
-  
+//   export default PowerBIEmbed;
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,5 @@
 import SearchBar from "../components/SearchBar";
 import MovieGrid from "../components/MovieGrid";
-import AnalyticsPanel from "../components/AnalyticsPanel";
-import TopLists from "../components/TopLists";
 import PowerBIEmbed from '../components/PowerBI';
 
 export default function Home() {
@@ -9,11 +7,25 @@ export default function Home() {
     <div className="min-h-screen bg-gray-900 text-white px-6 py-8">
       <h1 className="text-4xl font-bold mb-6 text-center">🎬 Movie Explorer</h1>
       <SearchBar />
-      <AnalyticsPanel />
-      <TopLists />
       <div className="my-8">
         <h2 className="text-2xl font-semibold mb-4 text-center">📊 Movie Data Dashboard</h2>
-        <PowerBIEmbed />
+        <PowerBIEmbed src="https://app.powerbi.com/reportEmbed?reportId=171f5c1b-20fa-4251-98a9-54d0604373db&autoAuth=true&ctid=108bc864-cdf5-4ec3-8b7c-4eb06be1b41d&pageName=c0e0826174f89ba5acd8&navContentPaneEnabled=false" />
+      </div>
+      <div className="my-8">
+        <h2 className="text-2xl font-semibold mb-4 text-center">📈 Movie Release Timeline</h2>
+        <PowerBIEmbed src="https://app.powerbi.com/reportEmbed?reportId=171f5c1b-20fa-4251-98a9-54d0604373db&autoAuth=true&ctid=108bc864-cdf5-4ec3-8b7c-4eb06be1b41d&pageName=19fbbac5ea0d134eedf8&navContentPaneEnabled=false" />
+      </div>
+      <div className="my-8">
+        <h2 className="text-2xl font-semibold mb-4 text-center">🏆 Top Rated Genre</h2>
+        <PowerBIEmbed src="https://app.powerbi.com/reportEmbed?reportId=171f5c1b-20fa-4251-98a9-54d0604373db&autoAuth=true&ctid=108bc864-cdf5-4ec3-8b7c-4eb06be1b41d&pageName=01388b741700e1cc9395&navContentPaneEnabled=false" />
+      </div>
+      <div className="my-8">
+        <h2 className="text-2xl font-semibold mb-4 text-center">🔥 Top Movies by Genre</h2>
+        <PowerBIEmbed src="https://app.powerbi.com/reportEmbed?reportId=171f5c1b-20fa-4251-98a9-54d0604373db&autoAuth=true&ctid=108bc864-cdf5-4ec3-8b7c-4eb06be1b41d&pageName=089f0ad2ab64b37d6166&navContentPaneEnabled=false" />
+      </div>
+      <div className="my-8">
+        <h2 className="text-2xl font-semibold mb-4 text-center">🔥 Top Movies by Original Language</h2>
+        <PowerBIEmbed src="https://app.powerbi.com/reportEmbed?reportId=171f5c1b-20fa-4251-98a9-54d0604373db&autoAuth=true&ctid=108bc864-cdf5-4ec3-8b7c-4eb06be1b41d&pageName=a24a5b2e617176494116&navContentPaneEnabled=false" />
       </div>
       <MovieGrid />
     </div>
